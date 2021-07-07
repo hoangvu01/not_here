f62efae551439154515aea231f3f96be05c2545e
HoangVu@pc <vuhoang1609@gmail.com>
2021-07-07 12:19:27 +0100

    Initial welcome page for the app with search box

---
+++ lib/home/dashboard.dart
@@ -0,0 +1,127 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class Dashboard extends StatelessWidget {
+  final Map<IconData, String> icons = Map.of({
+    Icons.local_police_outlined: "",
+    Icons.location_city: "",
+    Icons.location_searching: "",
+    Icons.warning: "",
+  });
+
+  Widget _iconDataToWidget(MapEntry entry) {
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 10),
+      padding: EdgeInsets.all(8),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10.0),
+        boxShadow: [
+          BoxShadow(
+            offset: Offset(0.0, 15.0),
+            color: Colors.blueGrey.shade100,
+            blurRadius: 10.0,
+            spreadRadius: 3.0,
+          ),
+        ],
+        color: Colors.white,
+      ),
+      child: Row(
+        children: [
+          SizedBox(
+            height: 50,
+            width: 50,
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(5),
+                color: Colors.blueGrey.shade300.withOpacity(0.5),
+              ),
+              child: Icon(
+                entry.key,
+                size: 25,
+                color: Colors.white,
+              ),
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.symmetric(vertical: 5),
+            padding: EdgeInsets.symmetric(horizontal: 5, vertical: 5),
+            child: Text(
+              entry.value,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Flexible(
+          flex: 2,
+          child: Center(
+            child: Container(
+              margin: EdgeInsets.symmetric(horizontal: 50),
+              child: Text(
+                'Are you travelling through a safe neighbourhood?',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ),
+        ),
+        Flexible(
+          flex: 1,
+          child: Container(
+            margin: EdgeInsets.symmetric(horizontal: 30.0),
+            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(30),
+              boxShadow: [
+                BoxShadow(
+                  offset: Offset(0.0, 15.0),
+                  color: Colors.blueGrey.shade100,
+                  blurRadius: 10.0,
+                  spreadRadius: 3.0,
+                ),
+              ],
+              color: Colors.white,
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Flexible(
+                  flex: 1,
+                  child: Icon(
+                    Icons.search,
+                  ),
+                ),
+                Flexible(
+                  flex: 6,
+                  child: TextField(
+                    decoration:
+                        InputDecoration(hintText: 'Town, city or postcode'),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+        Flexible(
+          flex: 4,
+          child: Container(
+            margin: EdgeInsets.symmetric(horizontal: 30.0, vertical: 20),
+            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            child: ListView(
+              children: icons.entries.map(_iconDataToWidget).toList(),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
