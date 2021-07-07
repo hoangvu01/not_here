44fcf43948548384aaae6b68c09fd1ce5037b8ea
HoangVu@pc <vuhoang1609@gmail.com>
2021-07-07 13:36:42 +0100

    Implement swipable curved navbar

--- lib/home/dashboard.dart
+++ lib/home/dashboard.dart
@@ -2,58 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
 class Dashboard extends StatelessWidget {
-  final Map<IconData, String> icons = Map.of({
-    Icons.local_police_outlined: "",
-    Icons.location_city: "",
-    Icons.location_searching: "",
-    Icons.warning: "",
-  });
-
-  Widget _iconDataToWidget(MapEntry entry) {
-    return Container(
-      margin: EdgeInsets.symmetric(vertical: 10),
-      padding: EdgeInsets.all(8),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(10.0),
-        boxShadow: [
-          BoxShadow(
-            offset: Offset(0.0, 15.0),
-            color: Colors.blueGrey.shade100,
-            blurRadius: 10.0,
-            spreadRadius: 3.0,
-          ),
-        ],
-        color: Colors.white,
-      ),
-      child: Row(
-        children: [
-          SizedBox(
-            height: 50,
-            width: 50,
-            child: Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(5),
-                color: Colors.blueGrey.shade300.withOpacity(0.5),
-              ),
-              child: Icon(
-                entry.key,
-                size: 25,
-                color: Colors.white,
-              ),
-            ),
-          ),
-          Container(
-            margin: EdgeInsets.symmetric(vertical: 5),
-            padding: EdgeInsets.symmetric(horizontal: 5, vertical: 5),
-            child: Text(
-              entry.value,
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -113,13 +61,7 @@ class Dashboard extends StatelessWidget {
         ),
         Flexible(
           flex: 4,
-          child: Container(
-            margin: EdgeInsets.symmetric(horizontal: 30.0, vertical: 20),
-            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-            child: ListView(
-              children: icons.entries.map(_iconDataToWidget).toList(),
-            ),
-          ),
+          child: Container(),
         ),
       ],
     );
